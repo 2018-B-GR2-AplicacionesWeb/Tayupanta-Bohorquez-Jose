5e6fe9e6e51fef72ce9e11f0fc79fbd0ad7eb477
josetabo21 <jose_btb@hotmail.com>
2018-10-10 15:06:48 -0500

    arreglos

---
+++ 01-javascript/02-arreglos.js
@@ -0,0 +1,30 @@
+
+var arreglosNumeros = [1,2,3];
+
+arreglosNumeros[0];
+arreglosNumeros[2];
+//arreglosNumeros.splice(indiceUsuario);
+console.log(arreglosNumeros);
+var usuario = 1.5;
+console.log(arreglosNumeros.indexOf(1));
+
+console.log(arreglosNumeros.slice(2,4));
+
+var arregloNotasPrimerBimestre = [8.5, 9, 4];
+var arregloNotasSegundoBimeste = [4, 5.7, 3];
+//destructuracion de arreglos
+var arregloNotas2018B = [...arregloNotasPrimerBimestre, ...arregloNotasSegundoBimeste];
+console.log(arregloNotas2018B);
+
+var adrian208A = {
+    sexualidad: 0,
+    web: 2
+};
+
+var adrian208B = {
+    MUSICA: 0,
+    MOVILES: 2
+};
+
+//funciones+
+
