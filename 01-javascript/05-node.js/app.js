143ec1b4e3af1c9e17ab827b0bee88d0aaefcd13
josetabo21 <jose_btb@hotmail.com>
2018-10-30 14:36:26 -0500

    asda

---
+++ 01-javascript/05-node.js/app.js
@@ -0,0 +1,4 @@
+const node = require('./nodeode.js');
+const so = require('./so.js');
+
+console.log(node);
---
+++ 01-javascript/05-node.js/so.js
@@ -0,0 +1,17 @@
+// so.js
+
+module.exports = {
+    tipo: 'privado',
+    activo: true,
+    version: 'windows 10',
+    imprimir: ()=>{
+        console.log(this.version);
+    },
+    archivos: [1, 2, 3],
+    programas: [
+        {
+            nombre: 'excel',
+            version: 2017
+        }
+    ]
+};
