bd265457a258b402b484225b5ad85bbed778ef91
josetabo21 <jose_btb@hotmail.com>
2018-11-07 15:42:58 -0500

    observables

--- 02-typescript/02-observables.js
+++ 02-typescript/02-observables.js
@@ -1,9 +1,51 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//02-observables.ts
+var operators_1 = require("rxjs/operators");
+var rxjs_1 = require("rxjs");
 var rxjs = require('rxjs');
+var map = require('rxjs/operators').map;
 var numeros$ = rxjs.of(1, 2, 3, 4, 5, 6);
-numeros$.subscribe(function (ok) {
+numeros$
+    .pipe(operators_1.distinct(), map(function (valorActal) {
+    return {
+        data: valorActal
+    };
+}))
+    .subscribe(function (ok) {
     console.log('en ok', ok);
 }, function (error) {
     console.log('error', error);
 }, function () {
     console.log('complete');
+});
+var promesa = function (funciona) {
+    return new Promise(function (resolve, reject) {
+        if (funciona) {
+            resolve(':)');
+        }
+        else {
+            reject(':(');
+        }
+    });
+};
+var promesa$ = rxjs.from(promesa(true));
+promesa$.subscribe(function (ok) {
+    console.log('promesa bien', ok);
+}, function (error) {
+    console.log('promesa mal', error);
+}, function () {
+    console.log('completado');
+});
+var observableConcatenado$ = numeros$.pipe(rxjs_1.concat(promesa$), operators_1.distinct(), map(function (valorActal) {
+    return {
+        data: valorActal
+    };
+}));
+observableConcatenado$.subscribe(function (ok) {
+    console.log('promesa bien', ok);
+}, function (error) {
+    console.log('promesa mal', error);
+}, function () {
+    console.log('completado');
 });
--- 02-typescript/02-observables.ts
+++ 02-typescript/02-observables.ts
@@ -1,10 +1,26 @@
 //02-observables.ts
-declare var require: any;
+import {distinct} from "rxjs/operators";
+import {concat} from "rxjs";
 
-const rxjs = require('rxjs');
+declare var require: any;
+declare var Promise: any;
+
+const rxjs = require('rxjs')
+const map = require('rxjs/operators').map;
 
 const numeros$ = rxjs.of(1,2,3,4,5,6);
-numeros$.subscribe(
+numeros$
+    .pipe(
+        distinct(),
+        map(
+            (valorActal)=>{
+                return{
+                    data: valorActal
+                }
+            }
+        )
+    )
+    .subscribe(
     (ok)=>{
         console.log('en ok', ok)
     },
@@ -13,5 +29,55 @@ numeros$.subscribe(
     },
     ()=>{
         console.log('complete')
+    }
+)
+
+const promesa = (funciona: boolean): Promise<string> => {
+    return new Promise(
+        (resolve, reject) => {
+            if(funciona){
+                resolve(':)')
+            } else {
+                reject(':(')
+            }
+        }
+    )
+}
+
+const promesa$ = rxjs.from(promesa(true))
+promesa$.subscribe(
+    (ok)=>{
+        console.log('promesa bien', ok)
+    },
+    (error)=>{
+        console.log('promesa mal', error)
+    },
+    ()=>{
+        console.log('completado')
+    }
+
+)
+
+const observableConcatenado$ = numeros$.pipe(
+    concat(promesa$),
+    distinct(),
+    map(
+        (valorActal)=>{
+            return{
+                data: valorActal
+            }
+        }
+    )
+)
+
+observableConcatenado$.subscribe(
+    (ok)=>{
+        console.log('promesa bien', ok)
+    },
+    (error)=>{
+        console.log('promesa mal', error)
+    },
+    ()=>{
+        console.log('completado')
     }
 )
